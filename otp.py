ed4408f4e4fc75dc7f6e74c69ce2358443b8f9f8
jseijo-p <jseijo-p@student.42malaga.com>
2022-08-01 16:20:16 +0200

    Validating

--- otp.py
+++ otp.py
@@ -3,6 +3,7 @@ import hmac
 import math
 from datetime import datetime
 
+
 class OTP(object):
     def __init__(self, secret: str):
         self.secret = secret
@@ -10,10 +11,11 @@ class OTP(object):
     def generate(self) -> str:
         presentDate = datetime.now()
         unix_timestamp = datetime.timestamp(presentDate)
-        current_time = unix_timestamp / 30.0;
+        current_time = int(unix_timestamp // 30)
         current_time = math.floor(current_time)
         current_time = str(current_time)
-        hash_bytes = hmac.digest(self.secret.encode(), current_time.encode(),hashlib.sha1)
+        print(current_time)
+        hash_bytes = hmac.digest(self.secret.encode('utf-8'), current_time.encode('utf-8'),hashlib.sha1)
         offset = hash_bytes[19] & 0xf
         password = 0
         password |= hash_bytes[offset] << 24
@@ -21,5 +23,6 @@ class OTP(object):
         password |= hash_bytes[offset + 2] << 8
         password |= hash_bytes[offset + 3]
         password = password % 1000000
+        print(password)
         min_len = lambda x: "{:06d}".format(x)
         return min_len(password)
